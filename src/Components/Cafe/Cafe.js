8e32f1636d7953541fb99b1a115d0f25124f7af4
tkremer <tbone7243@gmail.com>
2023-10-23 18:27:56 -0400

    Stopping for the day

--- src/Components/Cafe/Cafe.js
+++ src/Components/Cafe/Cafe.js
@@ -7,17 +7,22 @@ import BibleImg from '../../images/mobile-cafe/bible-and-cross-icon[152].png';
 
 
 const Cafe = () => {
-          return (<>
+          const LogoStyle = CafeStyle.Cafe.CafeStyle;
+          return (
+          <>
                     <div>
-                              <Image />
+                              <Image className="LogoStyle"/>
                               <h1>Services From</h1>
                               <h1>The Heart Of God</h1>
                               <div>
                                         <p>
-                                                  
+
                                         </p>
                               </div>
+                              <Image/>
+                              <button>Learn More</button>
                     </div>
-          </>);
+          </>
+          );
 }
 export default Cafe;
